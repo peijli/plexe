bab7b1a18e9d80c3d055405b4c8ff6c8df9caec4
Peijing Li <peijli@umich.edu>
2024-02-11 18:02:01 -0500

    create secure platooning beacon

--- src/plexe/protocols/SecurePlatooningBeaconing.cc
+++ src/plexe/protocols/SecurePlatooningBeaconing.cc
@@ -1,4 +1,19 @@
+#include <map>
 #include "SecurePlatooningBeaconing.h"
+#include "plexe/utilities/CryptoHelper.h"
+#include "plexe/utilities/JSONParser.h"
+
+#include "veins/modules/mac/ieee80211p/Mac1609_4.h"
+#include "veins/base/utils/FindModule.h"
+#include "veins/modules/messages/BaseFrame1609_4_m.h"
+
+#include "plexe/PlexeManager.h"
+#include "plexe/driver/Veins11pRadioDriver.h"
+#include "plexe/messages/PlexeInterfaceControlInfo_m.h"
+
+using namespace veins;
+
+// I think I have to overwrite a bunch of BaseApp methods...
 
 namespace plexe {
 
@@ -7,6 +22,15 @@ Define_Module(SecurePlatooningBeaconing)
 void SecurePlatooningBeaconing::initialize(int stage)
 {
     SimplePlatooningBeaconing::initialize(stage);
+    if (stage == 0) {
+        // generate encryption key
+        symmetricKey = CryptoHelper::generateSymmetricKey();
+    }
+    if (stage == 1) {
+        // print some useful information regarding the method and the vehicle
+        std::string msg = "SecurePlatooningBeaconing::initialize at vehicle " + std::to_string(positionHelper->getId());
+        getSimulation()->getEnvir()->alert(msg.c_str());
+    }
 }
 
 void SecurePlatooningBeaconing::handleSelfMsg(cMessage* msg)
@@ -21,5 +45,79 @@ SecurePlatooningBeaconing::SecurePlatooningBeaconing()
 SecurePlatooningBeaconing::~SecurePlatooningBeaconing()
 {
 }
+
+std::unique_ptr<BaseFrame1609_4> SecurePlatooningBeaconing::createBeacon(int destinationAddress) {
+    // vehicle's data to be included in the message
+    VEHICLE_DATA data;
+    // get information about the vehicle via traci
+    plexeTraciVehicle->getVehicleData(&data);
+
+    // create and send beacon
+    auto wsm = veins::make_unique<BaseFrame1609_4>("", BEACON_TYPE);
+    wsm->setRecipientAddress(LAddress::L2BROADCAST());
+    wsm->setChannelNumber(static_cast<int>(Channel::cch));
+    wsm->setUserPriority(priority);
+
+    // create platooning beacon with data about the car
+    PlatooningBeacon* pkt = new PlatooningBeacon();
+    pkt->setVehicleId(myId);
+    pkt->setControllerAcceleration(data.u);
+    pkt->setAcceleration(data.acceleration);
+    pkt->setSpeed(data.speed);
+    pkt->setPositionX(data.positionX);
+    pkt->setPositionY(data.positionY);
+    pkt->setTime(data.time);
+    pkt->setLength(length);
+    pkt->setSpeedX(data.speedX);
+    pkt->setSpeedY(data.speedY);
+    pkt->setAngle(data.angle);
+
+    // information not specific to the vehicle
+    auto nextSequenceNumber = seq_n++;
+    pkt->setKind(BEACON_TYPE);
+    // TODO: what is this packetSize?
+    pkt->setByteLength(packetSize);
+    pkt->setSequenceNumber(nextSequenceNumber);
+
+    SecurePlatooningBeacon* securePkt = encryptBeacon(pkt);
+
+    // wsm->encapsulate(securePkt);
+    wsm->encapsulate(pkt);
+
+    return wsm;
+}
+
+SecurePlatooningBeacon* SecurePlatooningBeaconing::encryptBeacon(const PlatooningBeacon* beacon) {
+    // create a SecurePlatooningBeacon object
+    SecurePlatooningBeacon* secureBeacon = new SecurePlatooningBeacon();
+
+    // convert the beacon to a map object
+    std::map<std::string, std::string> map;
+    map["vehicleId"] = std::to_string(beacon->getVehicleId());
+    map["controllerAcceleration"] = std::to_string(beacon->getControllerAcceleration());
+    map["acceleration"] = std::to_string(beacon->getAcceleration());
+    map["speed"] = std::to_string(beacon->getSpeed());
+    map["positionX"] = std::to_string(beacon->getPositionX());
+    map["positionY"] = std::to_string(beacon->getPositionY());
+    map["time"] = std::to_string(beacon->getTime());
+    map["length"] = std::to_string(beacon->getLength());
+    map["speedX"] = std::to_string(beacon->getSpeedX());
+    map["speedY"] = std::to_string(beacon->getSpeedY());
+    map["angle"] = std::to_string(beacon->getAngle());
+    // convert the map to a JSON string
+    std::string json = JSONParser::stringify(map);
+    // encrypt the JSON string
+    std::string encrypted = CryptoHelper::symmetricEncrypt(json, symmetricKey);
+    // set the properties of the secure beacon
+    secureBeacon->setEncryptedData(encrypted.c_str());
+    secureBeacon->setAlgorithm("AES");
+    secureBeacon->setKind(BEACON_TYPE);
+    // DO THESE EVEN WORK?
+    secureBeacon->setByteLength(beacon->getByteLength());
+    secureBeacon->setSequenceNumber(beacon->getSequenceNumber());
+    return secureBeacon;
+}
+
+
 
 } // namespace plexe
--- src/plexe/protocols/SecurePlatooningBeaconing.h
+++ src/plexe/protocols/SecurePlatooningBeaconing.h
@@ -3,15 +3,44 @@
 
 #include "BaseProtocol.h"
 #include "SimplePlatooningBeaconing.h"
+#include "plexe/utilities/CryptoHelper.h"
+#include "plexe/messages/PlatooningBeacon_m.h"
+#include "plexe/messages/SecurePlatooningBeacon_m.h"
+
+#include "veins/base/modules/BaseApplLayer.h"
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/messages/BaseFrame1609_4_m.h"
+
+#include "plexe/mobility/CommandInterface.h"
+#include "plexe/utilities/BasePositionHelper.h"
+
+#include "plexe/driver/PlexeRadioDriverInterface.h"
+
+#include <memory>
+#include <tuple>
 
 namespace plexe {
     class SecurePlatooningBeaconing : public SimplePlatooningBeaconing {
+    private:
+        // TODO: write this key to the plexe TraCI vehicle interface
+        std::string symmetricKey;
+        // overloaded method to check for duplicated secure beacons
+        bool isDuplicated(const SecurePlatooningBeacon* beacon);
+        // returns a SecurePlatooningBeacon from a PlatooningBeacon
+        SecurePlatooningBeacon* encryptBeacon(const PlatooningBeacon* beacon);
+        // returns a decrypted and parsed beacon from received SecurePlatooningBeacon
+        PlatooningBeacon* decryptBeacon(const SecurePlatooningBeacon* beacon);
     protected:
-        virtual void handleSelfMsg(cMessage* msg);
+        virtual void handleSelfMsg(cMessage* msg) override;
+        // overridden metnod to send platooning beacons
+        // void sendPlatooningMessage(int destinationAddress, )
+        // virtual void messageReceived(PlatooningBeacon* pkt, BaseFrame1609_4* frame) override;.
+        // virtual void duplicatedMessageReceived(PlatooningBeacon* pkt, BaseFrame1609_4* frame) override;
+        virtual std::unique_ptr<BaseFrame1609_4> createBeacon(int destinationAddress) override;
     public:
         SecurePlatooningBeaconing();
         virtual ~SecurePlatooningBeaconing();
-        virtual void initialize(int stage);
+        virtual void initialize(int stage) override;
     };
 } // namespace plexe
 

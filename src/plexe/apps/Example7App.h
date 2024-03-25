ef067daa0d54320e22ffda4f13227bf15ed6dae3
Peijing Li <peijli@umich.edu>
2024-03-25 13:40:35 -0400

    WTF

--- src/plexe/apps/Example7App.h
+++ src/plexe/apps/Example7App.h
@@ -1,4 +1,9 @@
-class Example7App: public BaseApp {
+#include "plexe/apps/BaseApp.h"
+#include "plexe/scenarios/BaseScenario.h"
+#include "plexe/messages/AbandonMessage_m.h"
+#include "plexe/CC_Const.h"
+
+class Example7App: public plexe::BaseApp {
 
 public:
     void sendAbandonMessage();
@@ -7,7 +12,7 @@ public:
 protected:
     virtual void initialize(int stage) override;
     virtual void handleLowerMsg(cMessage* msg) override;
-    BaseScenario* scenario;
+    plexe::BaseScenario* scenario;
 
 private:
     AbandonPlatoon* createAbandonMessage();

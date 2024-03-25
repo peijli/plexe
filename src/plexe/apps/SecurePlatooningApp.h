0ff54db748be1b869d3899d5e9394431829c26e3
Peijing Li <peijli@umich.edu>
2024-03-25 13:40:50 -0400

    Implement sending and receiving encrypted messages

--- src/plexe/apps/SecurePlatooningApp.h
+++ src/plexe/apps/SecurePlatooningApp.h
@@ -1,18 +1,22 @@
 #include "plexe/apps/BaseApp.h"
 #include "plexe/scenarios/BaseScenario.h"
 #include "plexe/CC_Const.h"
+#include "plexe/messages/SecureManeuverMessage_m.h"
 #include <string>
 
 namespace plexe {
     class SecurePlatooningApp : public plexe::BaseApp {
     public:
         virtual void sendUnicast(cPacket* msg, int destination);
+        void sendSecuredMessage(std::string message, int destination);
     protected:
         virtual void initialize(int stage) override;
         virtual void handleLowerMsg(cMessage* msg) override;
         plexe::BaseScenario* scenario;
     private:
         std::string symmetricKey;
+        SecureManeuverMessage* createSecureManeuverMessage(std::string message, std::string algorithm);
+        std::string handleSecureManeuverMessage(SecureManeuverMessage* msg);
         // std::string toString();
     };
 }
